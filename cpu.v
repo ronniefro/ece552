fd38c3a09e8529f7905919bf3c1ad57cf28de97c
Nordeast <dr.dimonddust@yahoo.com>
2014-11-06 00:39:29 -0600

    some cpu bull shit

--- cpu.v
+++ cpu.v
@@ -10,6 +10,165 @@ module cpu(input clk, rst_n,
 	
 	supply0 ZERO;
 	supply1 ONE;
+   wire[15:0] PC, INST, REG_A, REG_B, IMM; 
+   IM pcAddr(clk, pcAddress, rd_en, instruction);
+   rf READ (.clk(clk),.p0_addr(RS),.p1_addr(RT),.p0(REG_A),.p1(REG_B),.re0(ONE),.re1(ONE),.dst_addr(ZERO),.dst(ZERO),.we(ZERO),.hlt(ZERO));
+   alu ALU(.A(A), .B(B),.shift_amt(ID_EX_RT), .opcode(ID_EX_OP), .clk(clk),
+		      .S(OUT), .N(N), .Z(Z), .V(V));
+   /////// SIZE 16 regs ////////
+   reg[15:0] IF_ID_INST, ID_EX_INST, EX_MEM_INST, MEM_WB_INST, //instruction regs
+               ID_EX_A, ID_EX_B, EX_MEM_B, //alu operands
+                 EX_MEM_ALUR, MEM_WB_ALUR, MEM_WB_DATA, //alu and mem data
+                 
+   ////// control regs ////////
+   reg ID_EX_REGDST, ID_EX_ALUOP0, ID_EX_ALUOP1, ID_EX_ALUSRC, ID_EX_BRANCH, ID_EX_MEMREAD, ID_EX_MEMWRITE, ID_EX_REGWRITE, ID_EX_MEMTOREG, //ex stage controls
+         EX_MEM_BRANCH, EX_MEM_MEMREAD, EX_MEM_MEMWRITE, EX_MEM_REGWRITE, EX_MEM_MEMTOREG,
+           MEM_WB_REGWRITE, MEM_WB_MEMTOREG;
+   
+   ////// flag regs ///////
+   reg ZFLAG, NFLAG, VFLAG;   
+            
+   ////// SIZE 4 wires ////////
+   wire[3:0] ID_EX_RS, ID_EX_RT, EX_MEM_RD, MEM_WB_RD, MEM_WB_RT; // intermediate reg addrs
+   
+   wire[3:0] ID_EX_OP, EX_MEM_OP, MEM_WB_OP; // opcode wires
+   ///// size 16 ////////
+   wire[15:0] A,B; // alu wire parameters
+   
+   ////////// ID/EX ///////////
+   assign ID_EX_RS = ID_EX_INST[7:4];
+   assign ID_EX_RT = ID_EX_INST[3:0];
+   assign ID_EX_OP = ID_EX_INST[15:12];
+   
+   ////////// EX/MEM ///////////
+   assign EX_MEM_RD = EX_MEM_INST[11:8];
+   assign EX_MEM_OP = EX_MEM_INST[15:12];
+   assign EX_MEM_B = ID_EX_B;
+   
+   ////////// MEM/WB ///////////
+   assign MEM_WB_RT = MEM_WB_INST[3:0];
+   assign MEM_WB_RD = MEM_WB_INST[11:8];
+   assign MEM_WB_OP = MEM_WB_INST[15:12];
+   
+  ////////// ALU INPUT /////////// 
+   assign A = ID_EX_A;
+   assign B = ID_EX_B;
+   
+   
+
+   always@(posedge clk) begin
+       ////////IF operations /////// 
+       IF_ID_INST <= instruction;
+       PC <= PC + 4;
+       //////// END IF /////////////
+       
+       
+       ///////// ID OPERATIONS /////////
+       
+       IF_ID_A <= REG_A;
+       IF_ID_B <= REG_B;
+       ID_EX_INST <= IF_ID_INST;
+       
+       //set control options for sw,lw,llb,lhb
+       if(ID_EX_OP[3:2] = 2'b10) begin
+            ID_EX_REGDST <= 1'b1;
+            ID_EX_ALUOP0 <= 1'b1;
+            ID_EX_ALUOP1 <= 1'b1;
+            ID_EX_ALUSRC <= 1'b1;
+            ID_EX_BRANCH <= 1'b0;  
+            ID_EX_MEMTOREG <= 1'b1;
+            if(ID_EX_OP[1:0] = 2b'00)begin
+              // lw
+               ID_EX_MEMREAD <= 1'b1;
+               ID_EX_MEMWRITE <= 1'b0;
+               ID_EX_REGWRITE <= 1'b1;
+            end
+            else if(ID_EX_OP[1:0] = 2b'01)begin
+            // sw
+               ID_EX_MEMREAD <= 1'b0;
+               ID_EX_MEMWRITE <= 1'b1;
+               ID_EX_REGWRITE <= 1'b0;
+            end
+            else if(ID_EX_OP[1:0] = 2b'10 && ID_EX_OP[1:0] = 2b'11)begin
+            // llb/lhb
+               ID_EX_MEMREAD <= 1'b0;
+               ID_EX_MEMWRITE <= 1'b0;
+               ID_EX_REGWRITE <= 1'b1;
+            end
+       end
+       else if(ID_EX_OP[3] = 1'b0) begin
+           // r format
+            ID_EX_REGDST <= 1'b1;
+            ID_EX_ALUOP0 <= 1'b0;
+            ID_EX_ALUOP1 <= 1'b1;
+            ID_EX_ALUSRC <= 1'b0;
+            ID_EX_BRANCH <= 1'b0;  
+            ID_EX_MEMTOREG <= 1'b0;
+            ID_EX_MEMREAD <= 1'b0;
+            ID_EX_MEMWRITE <= 1'b0;
+            ID_EX_REGWRITE <= 1'b1;
+       end
+       else if(ID_EX_OP[3:2] = 2'b11) begin
+           // br,jal,jr,hlt
+           ID_EX_REGDST <= 1'b0;
+           ID_EX_ALUOP0 <= 1'b1;
+            ID_EX_ALUOP1 <= 1'b0;
+            ID_EX_BRANCH <= 1'b1;  
+            ID_EX_MEMTOREG <= 1'b0;
+            ID_EX_MEMREAD <= 1'b0;
+            ID_EX_MEMWRITE <= 1'b0;
+            if(ID_EX_OP[0] = 1'b0) begin
+                // br, jr
+               ID_EX_REGWRITE <= 1'b0;//
+               ID_EX_ALUSRC <= 1'b0;//
+            end
+            else if(ID_EX_OP[0] = 1'b1) begin
+                //jal
+               ID_EX_REGWRITE <= 1'b1;//
+               ID_EX_ALUSRC <= 1'b1;//
+            end
+       end
+       /// end control options /////////
+       ///// end ID ////////////////////
+       
+       
+       ///////// EX OPERATIONS /////////
+        //// pass controls ////
+       EX_MEM_BRANCH <= ID_EX_BRANCH;
+       EX_MEM_MEMREAD <= ID_EX_MEMREAD;
+       EX_MEM_MEMWRITE <= ID_EX_MEMWRITE;
+       EX_MEM_REGWRITE <= ID_EX_REGWRITE;
+       EX_MEM_MEMTOREG <= ID_EX_MEMTOREG;
+      
+      ///// alu operations /////
+      if(ID_EX_ALUOP0 == 1'b0 && ID_EX_ALUOP1 == 1'b0) begin
+          // sw and lw
+          EX_MEM_ALUR <= ID_EX_A +  IMM[3:0];
+          EX_MEM_B <= ID_EX_B; 
+      end
+      else begin
+         // r type ops
+         EX_MEM_ALUR = OUT;
+         ZFLAG = Z;
+         NFLAG = N;
+         VFLAG = V;
+      end
+  //////// MEM OPERATIONS /////////
+      
+      MEM_WB_REGWRITE <= EX_MEM_REGWRITE;
+      MEM_WB_MEMTOREG <= EX_MEM_MEMTOREG;
+   
+   
+   
+   
+   
+   
+   end
+   
+
+
+
+
 
 	reg [15:0] pcAddress;
 	reg [15:0] instruction;
@@ -22,8 +181,7 @@ module cpu(input clk, rst_n,
 	wire Z, N, V;
 	//rs = A, rt = B
 	wire[15:0] A,B;
-   alu ALU(.A(A), .B(B),.shift_amt(RT), .opcode(OPCODE), .clk(clk),
-		      .S(OUT), .N(N), .Z(Z), .V(V));
+   
    
    LLB LLB(.clk(clk), input[3:0] rd, input[7:0] imm, input hlt);	
    
@@ -49,8 +207,8 @@ module cpu(input clk, rst_n,
 	wire [3:0] RS, RT, RD;
 	wire [15:0] IMMEDIATE;
 
-	IM pcAddr(clk, pcAddress, rd_en, instruction);
-	rf READ (.clk(clk),.p0_addr(RS),.p1_addr(RT),.p0(A),.p1(B),.re0(ONE),.re1(ONE),.dst_addr(ZERO),.dst(ZERO),.we(ZERO),.hlt(ZERO));
+	
+	
 	rf WRITE(.clk(clk),.p0_addr(ZERO),.p1_addr(ONE),.p0(ZERO),.p1(ZERO),.re0(ZERO),.re1(ZERO),.dst_addr(RD),.dst(OUT),.we(ONE),.hlt(ZERO));
 
 	always @ (posedge clk or negedge rst_n) begin
